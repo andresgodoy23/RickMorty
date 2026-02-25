ea27b5c80dc9850a4d88cd43708d9017927bb5ba
api <andreslopez23512@gmail.com>
2026-02-24 20:59:47 -0600

    Initial Commit

---
+++ Characters/CharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersApp.swift
+//  Characters
+//
+//  Created by Andres Godoy on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
